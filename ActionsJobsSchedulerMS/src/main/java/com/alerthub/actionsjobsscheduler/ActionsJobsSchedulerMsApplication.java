339f6c007a5a9e8abc45c00d99b288b1f3aa3e4e
yazanateer <yazan.ateerr@gmail.com>
2025-11-20 20:45:18 +0200

    Initialized the AJS microservice

---
+++ ActionsJobsSchedulerMS/src/main/java/com/alerthub/actionsjobsscheduler/ActionsJobsSchedulerMsApplication.java
@@ -0,0 +1,13 @@
+package com.alerthub.actionsjobsscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionsJobsSchedulerMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionsJobsSchedulerMsApplication.class, args);
+	}
+
+}
---
+++ ActionsJobsSchedulerMS/src/test/java/com/alerthub/actionsjobsscheduler/ActionsJobsSchedulerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alerthub.actionsjobsscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsJobsSchedulerMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
